4be16893d2d59dee0d8f779784d7452a05f33fc2
Unknown <a.rndmized@gmail.com>
2018-03-15 17:30:29 +0000

    Updated comments in racket file.

--- maj/maj.rkt
+++ maj/maj.rkt
@@ -1,10 +1,13 @@
 #lang racket
 
-; Write a function hamming-distance in Racket that takes two lists and returns the
-; number of positions in which they differ. For example:
-;
-; > (hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
-; 5
+; Write a function maj in Racket that takes three lists x, y and z of equal length and
+; containing only 0’s and 1’s. It should return a list containing a 1 where two or more
+; of x, y and z contain 1’s, and 0 otherwise. 
+; 
+; For example:
+; 
+;  (maj (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+; '(0 0 0 1 0 1 1 1)
 
 (define (distance n1 n2 n3) ( if(> (+ n3 (+ n1 n2)) 1 ) 
                             1
